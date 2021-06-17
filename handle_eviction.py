c2d31074d6e1cdea02117c5f1ba16c8568acb44b
Roshan Bhandari <brishi98@gmail.com>
2021-06-17 15:38:58 -0400

    eviction handler update

--- handle_eviction.py
+++ handle_eviction.py
@@ -2,7 +2,8 @@
 
 import json
 import socket
-import urllib2
+# import urllib2
+import requests
 
 metadata_url = "http://169.254.169.254/metadata/scheduledevents?api-version=2019-08-01"
 this_host = socket.gethostname()
@@ -22,7 +23,7 @@ def test():
         cluster_name = 'Roshan'
         cluster_region = 'Roshan'
 
-        request.post(
+        requests.post(
             url,
             data={
                 'machine_started':machine_started,
@@ -37,31 +38,31 @@ def test():
 
         count += 1
 
-def get_scheduled_events():
-    req = urllib2.Request(metadata_url)
-    req.add_header('Metadata', 'true')
-    resp = urllib2.urlopen(req)
-    data = json.loads(resp.read())
-    return data
+# def get_scheduled_events():
+#     req = urllib2.Request(metadata_url)
+#     req.add_header('Metadata', 'true')
+#     resp = urllib2.urlopen(req)
+#     data = json.loads(resp.read())
+#     return data
 
 
-def handle_scheduled_events(data):
-    for evt in data['Events']:
-        eventid = evt['EventId']
-        status = evt['EventStatus']
-        resources = evt['Resources']
-        eventtype = evt['EventType']
-        resourcetype = evt['ResourceType']
-        notbefore = evt['NotBefore'].replace(" ", "_")
-        description = evt['Description']
-        eventSource = evt['EventSource']
-        if this_host in resources:
-            print("+ Scheduled Event. This host " + this_host +\
-                " is scheduled for " + eventtype + " by " + eventSource + \
-                " with description " + description +\
-                " not before " + notbefore)
+# def handle_scheduled_events(data):
+#     for evt in data['Events']:
+#         eventid = evt['EventId']
+#         status = evt['EventStatus']
+#         resources = evt['Resources']
+#         eventtype = evt['EventType']
+#         resourcetype = evt['ResourceType']
+#         notbefore = evt['NotBefore'].replace(" ", "_")
+#         description = evt['Description']
+#         eventSource = evt['EventSource']
+#         if this_host in resources:
+#             print("+ Scheduled Event. This host " + this_host +\
+#                 " is scheduled for " + eventtype + " by " + eventSource + \
+#                 " with description " + description +\
+#                 " not before " + notbefore)
 
-            # Add logic for handling events here
+#             # Add logic for handling events here
 
 def main():
     # data = get_scheduled_events()
