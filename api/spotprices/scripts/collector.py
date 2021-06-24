3999e996fe223f09cadadf49a78ead345200c710
Roshan Bhandari <brishi98@gmail.com>
2021-06-24 14:48:39 -0400

    fix price pulling script

--- api/spotprices/scripts/collector.py
+++ api/spotprices/scripts/collector.py
@@ -9,8 +9,7 @@ from spotprices.models import SpotPrices
 
 
 def pull_price_using_api(now_time):
-    final_sizes = [size.replace('Standard_', '').replace('_', ' ') for size in sizes]
-    result = pull_mupliple_api(region_codes, final_sizes, now_time)
+    result = pull_mupliple_api(now_time)
     return result
 
 def pull_price_using_cli(now_time):
@@ -19,45 +18,23 @@ def pull_price_using_cli(now_time):
     return result
 
 def save_price_to_database(api_price, cli_price, method): 
-    if method.lower() == 'both':
-        for each_api_price in api_price:
-            for each_cli_price in cli_price:
-                if each_cli_price['size'].replace('Standard_', '').replace('_', ' ') == each_api_price['size'] and \
-                    each_api_price['region'] == each_cli_price['region'].replace(resource_prefix, ''):
-                    sp = SpotPrices(
-                            time=each_api_price['time'],
-                            region=each_api_price['region'],
-                            size=each_cli_price['size'], 
-                            api_price=each_api_price['api_price'], 
-                            cli_price=each_cli_price['cli_price'],
-                            pay_as_you_go_price=each_api_price['pay_as_you_go_price'],
-                            one_year_reserved_price=each_api_price['1_year_reserved_price'],
-                            three_year_reserved_price=each_api_price['3_year_reserved_price'],
-                            per_saving_paygo=each_api_price['%_saving_pay_as_you_go'],
-                            per_saving_one=each_api_price['%_saving_1y_reserved'],
-                            per_saving_three=each_api_price['%_saving_3y_reserved']
-                    )
+    datas = api_price or cli_price
+    for data in datas:
+        sp = SpotPrices(
+                time=data['time'],
+                region=data['region'],
+                size=data['size'], 
+                api_price=data['api_price'], 
+                cli_price=data['cli_price'],
+                pay_as_you_go_price=data['pay_as_you_go_price'],
+                one_year_reserved_price=data['1_year_reserved_price'],
+                three_year_reserved_price=data['3_year_reserved_price'],
+                per_saving_paygo=data['%_saving_pay_as_you_go'],
+                per_saving_one=data['%_saving_1y_reserved'],
+                per_saving_three=data['%_saving_3y_reserved']
+        )
 
-                    sp.save()
-
-    else:
-        datas = api_price or cli_price
-        for data in datas:
-            sp = SpotPrices(
-                    time=data['time'],
-                    region=data['region'],
-                    size=data['size'], 
-                    api_price=data['api_price'], 
-                    cli_price=data['cli_price'],
-                    pay_as_you_go_price=data['pay_as_you_go_price'],
-                    one_year_reserved_price=data['1_year_reserved_price'],
-                    three_year_reserved_price=data['3_year_reserved_price'],
-                    per_saving_paygo=data['%_saving_pay_as_you_go'],
-                    per_saving_one=data['%_saving_1y_reserved'],
-                    per_saving_three=data['%_saving_3y_reserved']
-            )
-
-            sp.save()
+        sp.save()
 
 def run():
     method = "api"
@@ -70,10 +47,6 @@ def run():
         api_price = pull_price_using_api(now_time)
 
     elif method.lower() == "cli":
-        cli_price = pull_price_using_cli(now_time)
-    
-    else:
-        api_price = pull_price_using_api(now_time)
         cli_price = pull_price_using_cli(now_time)
 
     save_price_to_database(api_price, cli_price, method)
--- api/spotprices/scripts/price_puller_azure_api.py
+++ api/spotprices/scripts/price_puller_azure_api.py
@@ -1,91 +1,89 @@
+import time
 import requests
 import datetime
 from multiprocessing.pool import ThreadPool
-from .config import n_threads
+from .config import n_threads, spot_region_map
 
 
-multi_result = []
-def pull_price(region_name, instance_size, now_time=str(datetime.datetime.now()), is_windows_instance=False):
-    region_name = region_name
-    instance_size = instance_size
+def call_end_point(url):
+    return requests.get(url).json()['Items']
+
+multi_result = {}
+def pull_price(region_name, sizes, start, end, now_time=str(datetime.datetime.now()), is_windows_instance=False):
     is_windows_instance = is_windows_instance
-    spot_end_point = "https://prices.azure.com/api/retail/prices?$filter=skuName eq '" + \
-                    str(instance_size) + \
-                    " Spot' and meterName eq '" + \
-                    str(instance_size) + \
-                    " Spot' and armRegionName eq '" + \
-                    str(region_name) + \
-                    "'"
+ 
+    data = []
+    end_point = "https://prices.azure.com/api/retail/prices?$filter="
+    params = ''
+    for count, instance_size in enumerate(sizes):
+        params += "skuName eq '" + str(instance_size).replace('Standard_', '').replace('_', ' ') + " Spot' or "
+        if (count+1)%19 == 0:
+            spot_end_point = end_point + params[:-3] + "and armRegionName eq '" + str(region_name) + "'" 
+            data += call_end_point(spot_end_point)
+            time.sleep(5)
+            params = ''
 
-    regular_end_point = "https://prices.azure.com/api/retail/prices?$filter=skuName eq '" + \
-                    str(instance_size) + \
-                    "' and meterName eq '" + \
-                    str(instance_size) + \
-                    "' and armRegionName eq '" + \
-                    str(region_name) + \
-                    "'"
+    if params:
+        spot_end_point = end_point + params[:-3] + "and armRegionName eq '" + str(region_name) + "'" 
+        data += call_end_point(spot_end_point) 
 
-    regular_data = requests.get(regular_end_point).json()
-    spot_data = requests.get(spot_end_point).json() 
-    return_data = [now_time, region_name, instance_size, float('-inf'), float('-inf'), float('-inf'), float('-inf')]
+    params = ''
+    for count, instance_size in enumerate(sizes):
+        params += "skuName eq '" + str(instance_size).replace('Standard_', '').replace('_', ' ') + "' or "
+        if (count+1)%19 == 0:
+            regular_end_point = end_point + params[:-3] + "and armRegionName eq '" + str(region_name) + "'" 
+            data += call_end_point(regular_end_point)
+            time.sleep(5)
+            params = ''
 
-    for each_instance in spot_data['Items']:
-        if not is_windows_instance and 'windows' not in each_instance['productName'].lower():
-            return_data = [
-                now_time, 
-                region_name, 
-                instance_size, 
-                min(each_instance['unitPrice'], each_instance['retailPrice']),
-                float('-inf'),
-                float('-inf'),
-                float('-inf')
-            ]
+    if params:
+        regular_end_point = end_point + params[:-3] + "and armRegionName eq '" + str(region_name) + "'" 
+        data += call_end_point(regular_end_point)
+    
+    if region_name not in multi_result:
+        multi_result [region_name] = {}
 
-        elif is_windows_instance and 'windows' in each_instance['productName'].lower():
-            return_data = [
-                now_time,
-                region_name,
-                instance_size,
-                min(each_instance['unitPrice'],each_instance['retailPrice']),
-                float('-inf'),
-                float('-inf'),
-                float('-inf')
-            ]
+    for each_instance in data:
+        instance_size = each_instance['armSkuName']
 
-    for each_instance in regular_data['Items']:
+        if instance_size not in multi_result[region_name]:
+            multi_result[region_name][instance_size] = [now_time, region_name, instance_size, float('-inf'), float('-inf'), float('-inf'), float('-inf')]
+
+        if not is_windows_instance and 'windows' not in each_instance['productName'].lower() and \
+            'spot' in each_instance['meterName'].lower() and each_instance['type'] == 'Consumption':
+            multi_result[region_name][instance_size][3] = each_instance['unitPrice']
+ 
+        if not is_windows_instance and 'windows' not in each_instance['productName'].lower() and \
+            'spot' not in each_instance['meterName'].lower() and each_instance['type'] == 'Consumption':
+            multi_result[region_name][instance_size][4] = each_instance['unitPrice']
+
         if not is_windows_instance and 'windows' not in each_instance['productName'].lower() and \
             each_instance['type'] == 'Reservation' and each_instance['reservationTerm'] == '1 Year':
-            return_data[4] = each_instance['unitPrice']/(365*24)
+            multi_result[region_name][instance_size][5] = each_instance['unitPrice']/(365*24)
 
-        elif not is_windows_instance and 'windows' not in each_instance['productName'].lower() and \
+        if not is_windows_instance and 'windows' not in each_instance['productName'].lower() and \
             each_instance['type'] == 'Reservation' and each_instance['reservationTerm'] == '3 Years':
-            return_data[5] = each_instance['unitPrice']/(365*24)
+            multi_result[region_name][instance_size][6] = each_instance['unitPrice']/(365*24)
+ 
+def process_savings(data): 
+    per_saving_pay_as_you_go = float('-inf')
+    if data[4] and data[3]:
+        per_saving_pay_as_you_go = (data[4] - data[3])/data[4]*100
+    
+    per_saving_1y_reserved = float('-inf')
+    if data[5] and data[3]:
+        per_saving_1y_reserved = (data[5] - data[3])/data[5]*100
+    
+    per_saving_3y_reserved = float('-inf')
+    if data[6] and data[3]:
+        per_saving_3y_reserved = (data[6] - data[3])/data[6]*100
 
-        elif not is_windows_instance and 'windows' not in each_instance['productName'].lower() and \
-            each_instance['type'] == 'Consumption':
-            return_data[6] = each_instance['unitPrice']
-
-    return return_data
-
-def callback(data):
-    if data: 
-        per_saving_pay_as_you_go = float('-inf')
-        if data[6] and data[3]:
-            per_saving_pay_as_you_go = (data[6] - data[3])/data[6]*100
-        
-        per_saving_1y_reserved = float('-inf')
-        if data[4] and data[3]:
-            per_saving_1y_reserved = (data[4] - data[3])/data[4]*100
-        
-        per_saving_3y_reserved = float('-inf')
-        if data[5] and data[3]:
-            per_saving_3y_reserved = (data[5] - data[3])/data[5]*100
-
-        multi_result.append({
+    if data[3] > 0 and data[4] > 0:
+        return {
             'time': data[0],
             'region': data[1],
             'size': data[2],
-            'api_price': data[3] if data[3] else float('-inf'),
+            'api_price': data[3],
             'cli_price': float('-inf'),
             'pay_as_you_go_price': data[4], 
             '1_year_reserved_price': data[5],
@@ -93,18 +91,27 @@ def callback(data):
             '%_saving_pay_as_you_go': per_saving_pay_as_you_go,
             '%_saving_1y_reserved': per_saving_1y_reserved,
             '%_saving_3y_reserved': per_saving_3y_reserved
-        })
+        }
 
-def pull_multiple(region_names, instance_sizes, now_time, is_windows_instances=False):
+def pull_multiple(now_time, is_windows_instances=False):
     global multi_result
-    multi_result = []
+    multi_result = {}
+
 
     p = ThreadPool(n_threads)
-    for each_region in region_names:
-        for each_instance in instance_sizes:
-            price = p.apply_async(pull_price, args=(each_region, each_instance, now_time, is_windows_instances, ), callback=callback) 
-        
+
+    return_data = []
+    count = 0
+    for region in spot_region_map.keys():
+        price = p.apply_async(pull_price, args=(region, spot_region_map[region], now_time, is_windows_instances, ))
+
     p.close()
     p.join()
 
-    return multi_result
+    for region  in multi_result.keys():
+        for size in multi_result[region].keys(): 
+            result = process_savings(multi_result[region][size])
+            if result:
+                return_data.append(result)
+
+    return return_data
